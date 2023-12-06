44217560035ffdc828dcc1171360a28e2736b972
mauricekoller <maurice-koller@online.de>
2023-12-06 11:20:11 +0100

    faster solution for part 2

--- day_06/aoc_day_06.py
+++ day_06/aoc_day_06.py
@@ -1,4 +1,5 @@
 from functools import reduce
+import math
 
 
 def part_one(lines):
@@ -19,21 +20,22 @@ def part_one(lines):
     print(f"Sum Part One: {mult}")
 
 
+def Mitternachtsformel(a, b, c):
+    x = (-b + math.sqrt(b**2 - 4*a*c))/2*a
+    y = (-b - math.sqrt(b**2 - 4*a*c))/2*a
+    return x, y
+
+
 def part_two(lines):
     resultset = []
     times, distances = [[val for val in line.split(':')[1].split()] for line in lines]
     winning_oppotunities = 0
     max_time = int(''.join(times))
     min_distance = int(''.join(distances))
-    for duration in range(max_time):
-        speed = duration
-        distance = (max_time-duration)*speed
-        if (distance > min_distance):
-            winning_oppotunities += 1
-    resultset.append(winning_oppotunities)
-    mult = reduce(lambda a, b: a*b, resultset)
-
-    print(f"Sum Part Two: {mult}")
+    x, y = Mitternachtsformel(-1, max_time, -min_distance)
+    lower_bound = math.ceil(min(x, y))
+    upper_bound = math.floor(max(x, y))
+    print(f"Sum Part Two: {upper_bound-lower_bound+1}")
 
 
 with open("day_06/input.txt", "r") as input:
